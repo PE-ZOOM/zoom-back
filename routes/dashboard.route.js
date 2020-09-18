5f5a69607915b5c19562277d782675554274775f
RAPHAEL <nicarap@hotmail.com>
2020-09-18 08:28:44 +0400

    dashboard route - ké pu kossa ma fé

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -5,40 +5,69 @@ const connection_pool = require('../db2');
 
 router.get('/jalon', (req, res) => {
   const query = req.query;
-  const int1= [0,30];
-  const int2= [int1[1] + 1, 60];
-  let sql = 'SELECT x.dc_lblmotifjalonpersonnalise,'    
-  sql+=` MAX(CASE WHEN x.textnbjouravantjalon = "Sans Jalons" THEN x.nb ELSE 0 END) "Sans Jalons",`
-  sql+=` MAX(CASE WHEN x.textnbjouravantjalon = "Jalons dépassés" THEN x.nb ELSE 0 END) "Jalons dépassés",`
-  sql+=` MAX(CASE WHEN x.textnbjouravantjalon = "Entre ${int1[0]} et ${int1[1]} jours" THEN x.nb ELSE 0 END) "Entre ${int1[0]} et ${int1[1]} jours",`
-  sql+=` MAX(CASE WHEN x.textnbjouravantjalon = "Entre ${int2[0]} et ${int2[1]} jours" THEN x.nb ELSE 0 END) "Entre ${int2[0]} et ${int2[1]} jours",`
-  sql+=` MAX(CASE WHEN x.textnbjouravantjalon = "> ${int2[1]} jours" THEN x.nb ELSE 0 END) "> ${int2[1]} jours",`
-  sql+=` SUM(x.nb) Total`
-  sql+=' FROM ('
+  // const int1= [0,30];
+  // const int2= [int1[1] + 1, 60];
+  // let sql = 'SELECT x.dc_lblmotifjalonpersonnalise,'    
+  // sql+=` MAX(CASE WHEN x.textnbjouravantjalon = "Sans Jalons" THEN x.nb ELSE 0 END) "Sans Jalons",`
+  // sql+=` MAX(CASE WHEN x.textnbjouravantjalon = "Jalons dépassés" THEN x.nb ELSE 0 END) "Jalons dépassés",`
+  // sql+=` MAX(CASE WHEN x.textnbjouravantjalon = "Entre ${int1[0]} et ${int1[1]} jours" THEN x.nb ELSE 0 END) "Entre ${int1[0]} et ${int1[1]} jours",`
+  // sql+=` MAX(CASE WHEN x.textnbjouravantjalon = "Entre ${int2[0]} et ${int2[1]} jours" THEN x.nb ELSE 0 END) "Entre ${int2[0]} et ${int2[1]} jours",`
+  // sql+=` MAX(CASE WHEN x.textnbjouravantjalon = "> ${int2[1]} jours" THEN x.nb ELSE 0 END) "> ${int2[1]} jours",`
+  // sql+=` SUM(x.nb) Total`
+  // sql+=' FROM ('
 
-    sql += 'SELECT dc_lblmotifjalonpersonnalise,'
-    sql += ' CASE'
-    sql += ' WHEN nbjouravantjalon IS NULL THEN "Sans Jalons"'
-    sql += ' WHEN nbjouravantjalon < 0 THEN "Jalons dépassés"'
-    sql += ` WHEN nbjouravantjalon BETWEEN ${int1[0]} AND ${int1[1]} THEN "Entre ${int1[0]} et ${int1[1]} jours"`
-    sql += ` WHEN nbjouravantjalon BETWEEN ${int2[0]} AND ${int2[1]} THEN "Entre ${int2[0]} et ${int2[1]} jours"`
-    sql += ` WHEN nbjouravantjalon > ${int2[1]} THEN "> ${int2[1]} jours"`
-    sql += ' ELSE "jalons"'
-    sql += ' END AS textnbjouravantjalon,'
-    sql += ' COUNT(dc_individu_local) AS nb'
-    sql += ' FROM T_Portefeuille INNER JOIN APE ON T_Portefeuille.dc_structureprincipalede = APE.id_ape'
-    sql += ' WHERE dc_situationde = 2'
+  //   sql += 'SELECT dc_lblmotifjalonpersonnalise,'
+  //   sql += ' CASE'
+  //   sql += ' WHEN nbjouravantjalon IS NULL THEN "Sans Jalons"'
+  //   sql += ' WHEN nbjouravantjalon < 0 THEN "Jalons dépassés"'
+  //   sql += ` WHEN nbjouravantjalon BETWEEN ${int1[0]} AND ${int1[1]} THEN "Entre ${int1[0]} et ${int1[1]} jours"`
+  //   sql += ` WHEN nbjouravantjalon BETWEEN ${int2[0]} AND ${int2[1]} THEN "Entre ${int2[0]} et ${int2[1]} jours"`
+  //   sql += ` WHEN nbjouravantjalon > ${int2[1]} THEN "> ${int2[1]} jours"`
+  //   sql += ' ELSE "jalons"'
+  //   sql += ' END AS textnbjouravantjalon,'
+  //   sql += ' COUNT(dc_individu_local) AS nb'
+  //   sql += ' FROM T_Portefeuille INNER JOIN APE ON T_Portefeuille.dc_structureprincipalede = APE.id_ape'
+  //   sql += ' WHERE dc_situationde = 2'
 
-  Object.keys(query).filter((key) => query[key]!=='all').map((key) => {
-    if(req.query[key]!=="null" && req.query[key]!==undefined)
-    {
-      sql += ` AND ${key} = "${req.query[key]}" `
-    }
-  })
+  // Object.keys(query).filter((key) => query[key]!=='all').map((key) => {
+  //   if(req.query[key]!=="null" && req.query[key]!==undefined)
+  //   {
+  //     sql += ` AND ${key} = "${req.query[key]}" `
+  //   }
+  // })
 
-  sql += ' GROUP BY dc_lblmotifjalonpersonnalise, textnbjouravantjalon) x'
-  sql += ' GROUP BY x.dc_lblmotifjalonpersonnalise'
+  // sql += ' GROUP BY dc_lblmotifjalonpersonnalise, textnbjouravantjalon) x'
+  // sql += ' GROUP BY x.dc_lblmotifjalonpersonnalise'
 
+    // sql = 'SELECT' 
+    // sql += ' MAX(CASE WHEN x.lbl = "Sans Jalons" THEN x.nb ELSE 0 END) "Sans Jalons", '
+    // sql += ' MAX(CASE WHEN x.lbl = "Jalons dépassés" THEN x.nb ELSE 0 END) "Jalons dépassés", '
+    // sql += ' MAX(CASE WHEN x.lbl = "Entre 0 et 30 jours" THEN x.nb ELSE 0 END) "Entre 0 et 30 jours"'
+
+    // sql += ' FROM ('
+    let sql = '    SELECT '
+    sql += '          CASE WHEN nbjouravantjalon IS NULL THEN "Sans Jalons" '
+    sql += '                WHEN nbjouravantjalon < 0 THEN "Jalons dépassés" '
+    sql += '                WHEN nbjouravantjalon BETWEEN 0 AND 30 THEN "Entre 0 et 30 jours" '
+    sql += '          END AS lbl,'
+    sql += '                  COUNT(dc_individu_local) AS nb'
+    sql += '           '
+    sql += '          FROM T_Portefeuille '
+    sql += '          INNER JOIN APE ON T_Portefeuille.dc_structureprincipalede = APE.id_ape '
+    sql += '          WHERE dc_situationde = 2 '
+
+          Object.keys(query).filter((key) => query[key]!=='all').map((key) => {
+            if(req.query[key]!=="null" && req.query[key]!==undefined)
+            {
+              sql += ` AND ${key} = "${req.query[key]}" `
+            }
+          })
+
+    sql += '          GROUP BY lbl'
+    // sql += '           ) x'
+
+
+  
   connection_pool.getConnection(function(error, conn) {
     if (error) throw err; // not connected!
 
@@ -48,7 +77,7 @@ router.get('/jalon', (req, res) => {
 
       // Handle error after the release.
       if (err){
-        console.log(err.sqlMessage)
+        console.log(err)
         return  res.status(500).json({
                 err: "true", 
                 error: err.message,
@@ -69,7 +98,7 @@ router.get('/jalon', (req, res) => {
 router.get('/efo', (req, res) => {
   const query = req.query;
 
-  let sql = 'SELECT COUNT(dc_statutaction_id) as Qte, dc_statutaction_id FROM T_EFO'
+  let sql = 'SELECT CONCAT("EFO ", dc_statutaction_id)  as lbl, COUNT(dc_statutaction_id) as nb FROM T_EFO'
     sql += ' INNER JOIN APE ON T_EFO.dc_structureprincipalede = APE.id_ape'
 
   // Object.keys(query).map((key, index) => {
@@ -116,7 +145,7 @@ router.get('/efo', (req, res) => {
 router.get('/activite', (req, res) => {
   const query = req.query;
 
-  let sql = 'SELECT COUNT(*) as NbDESansActivite FROM `t_portefeuille` WHERE nbjoursansentretien > 360 AND nbjoursanscontactsortantteloumel > 360'
+  let sql = 'SELECT "Nb DE sans activités" as lbl, COUNT(*) as nb FROM `t_portefeuille` WHERE nbjoursansentretien > 360 AND nbjoursanscontactsortantteloumel > 360'
 
   Object.keys(query).map((key, index) => {
     sql += ` AND ${key} = "${query[key]}"  `
