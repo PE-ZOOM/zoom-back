01b80956a296b73ac23397b5aecafe2eb4d22332
Raph974 <68837511+nicarap@users.noreply.github.com>
2020-11-24 16:37:51 +0400

    r_graph_bubble

--- routes/load.route.js
+++ routes/load.route.js
@@ -547,71 +547,335 @@ router.get('/historic', passport.authenticate('jwt', { session:  false }), (req,
 });
 
 
-// router.get('/historicS', (req, resp) => {
-//     let sql = 'SELECT COUNT(*), button as bt'
-//         sql += ' FROM historic WHERE WEEK(date) = WEEK(curdate()) AND MONTH(date) = MONTH(CURDATE())'
-//         sql += ' AND NOT idgasi = "a"'
-//         sql += ' GROUP BY button'
-//     // connection.query('SELECT COUNT(button) as Jalon FROM historic WHERE button = "Jalons"', (err, results) => {
-//   connection_pool.getConnection(function(error, conn) {
-//     if (error) throw err; // not connected!
+router.get('/historicBubble', (req, resp) => {
 
-//     // Use the connection
-//     conn.query(sql, (err, result) => {
-//       // When done with the connection, release it.
-//       conn.release();
+  // let sql = 'SELECT MONTH(date) AS mois, button AS BT, COUNT(*) as NB FROM historic GROUP BY mois, BT '
 
-//       // Handle error after the release.
-//       if (err) {
-//         return  resp.status(500).json({
-//                 err: "true", 
-//                 error: err.message,
-//                 errno: err.errno,
-//                 sql: err.sql,
-//                 });
-//       }
-//       else {
-//         return  resp
-//                 .status(201)
-//                 .json(result);
-//       }
-//     })
-//   });
+  let sql = 'SELECT a.mois, '
+    sql += 'a.Contacts, p.ContactsPersonne,'
+    sql += 'a.Diagnostic, p.DiagnosticPersonne,'
+    sql += 'a.EFO, p.EFOPersonne,'
+    sql += 'a.Jalons, p.JalonsPersonne,'
+    sql += 'a.Taux, p.TauxPersonne,'
+    sql += 'a.Prestations, p.PrestationsPersonne FROM '
+    sql += ' ('
+    sql += '    SELECT c.mois,'
+    sql += '        CASE WHEN y.Contacts IS NULL THEN 0 ELSE y.Contacts END "Contacts", '
+    sql += '        CASE WHEN y.Diagnostic IS NULL THEN 0 ELSE y.Diagnostic END "Diagnostic", '
+    sql += '        CASE WHEN y.EFO IS NULL THEN 0 ELSE y.EFO END "EFO", '
+    sql += '        CASE WHEN y.Jalons IS NULL THEN 0 ELSE y.Jalons END "Jalons", '
+    sql += '        CASE WHEN y.Taux IS NULL THEN 0 ELSE y.Taux END "Taux", '
+    sql += '        CASE WHEN y.Prestations IS NULL THEN 0 ELSE y.Prestations END "Prestations"'
+    sql += '    FROM '
+    sql += '    ('
+    sql += '          SELECT x.mois,'
+    sql += '              MAX(CASE WHEN x.button = "Contacts" THEN x.nbclic ELSE 0 END) "Contacts",'
+    sql += '              MAX(CASE WHEN x.button = "Diagnostic" THEN x.nbclic ELSE 0 END) "Diagnostic",'
+    sql += '              MAX(CASE WHEN x.button = "EFO" THEN x.nbclic ELSE 0 END) "EFO",'
+    sql += '             MAX(CASE WHEN x.button = "Jalons" THEN x.nbclic ELSE 0 END) "Jalons",'
+    sql += '             MAX(CASE WHEN x.button = "Taux" THEN x.nbclic ELSE 0 END) "Taux",'
+    sql += '             MAX(CASE WHEN x.button = "Prestations" THEN x.nbclic ELSE 0 END) "Prestations"'
+    sql += '         FROM'
+    sql += '         ('
+    sql += '              SELECT MONTH(date) as mois, button, count(id) as nbclic FROM historic '
+    sql += '             WHERE idgasi NOT in ("icbl0540")'
+    sql += '             AND YEAR(date)=2020 '
+    sql += '             GROUP BY mois, button) x'
+    sql += '             GROUP BY x.mois'
+    sql += '         ) y '
+    sql += '         RIGHT JOIN historic_calc c on c.mois= y.mois'
+    sql += '         WHERE c.mois <= ('
+    sql += '            SELECT MAX(MONTH(date)) FROM historic '
+    sql += '            WHERE YEAR(date)=2020'
+    sql += '          )'
+    sql += '          AND c.mois >= ('
+    sql += '             SELECT MIN(MONTH(date)) FROM historic '
+    sql += '             WHERE YEAR(date)=2020'
+    sql += '           ) ' 
+    sql += '    ) a'
+                                                  
+    sql += '    INNER JOIN '
+    sql += '    ('
+    sql += '        SELECT c.mois,'
+    sql += '            CASE WHEN y.Contacts IS NULL THEN 0 ELSE y.Contacts END "ContactsPersonne", '
+    sql += '            CASE WHEN y.Diagnostic IS NULL THEN 0 ELSE y.Diagnostic END "DiagnosticPersonne", '
+    sql += '            CASE WHEN y.EFO IS NULL THEN 0 ELSE y.EFO END "EFOPersonne", '
+    sql += '            CASE WHEN y.Jalons IS NULL THEN 0 ELSE y.Jalons END "JalonsPersonne", '
+    sql += '            CASE WHEN y.Taux IS NULL THEN 0 ELSE y.Taux END "TauxPersonne", '
+    sql += '            CASE WHEN y.Prestations IS NULL THEN 0 ELSE y.Prestations END "PrestationsPersonne"'
+    sql += '        FROM '
+    sql += '        ('
+    sql += '          SELECT x.mois,'
+    sql += '              MAX(CASE WHEN x.button = "Contacts" THEN x.nbclic ELSE 0 END) "Contacts",'
+    sql += '              MAX(CASE WHEN x.button = "Diagnostic" THEN x.nbclic ELSE 0 END) "Diagnostic",'
+    sql += '              MAX(CASE WHEN x.button = "EFO" THEN x.nbclic ELSE 0 END) "EFO",'
+    sql += '              MAX(CASE WHEN x.button = "Jalons" THEN x.nbclic ELSE 0 END) "Jalons",'
+    sql += '              MAX(CASE WHEN x.button = "Taux" THEN x.nbclic ELSE 0 END) "Taux",'
+    sql += '              MAX(CASE WHEN x.button = "Prestations" THEN x.nbclic ELSE 0 END) "Prestations"'
+    sql += '          FROM'
+    sql += '          ('
+    sql += '              SELECT MONTH(date) as mois, button, count(DISTINCT(idgasi)) as nbclic FROM historic '
+    sql += '              WHERE idgasi NOT in ("icbl0540")'
+    sql += '              AND YEAR(date)=2020 '
+    sql += '              GROUP BY mois, button) x'
+    sql += '              GROUP BY x.mois'
+    sql += '           ) y RIGHT JOIN historic_calc c on c.mois= y.mois'
+    sql += '           WHERE c.mois <= ('
+    sql += '              SELECT MAX(MONTH(date)) FROM historic '
+    sql += '              WHERE YEAR(date)=2020'
+    sql += '           )'
+    sql += '          AND c.mois >= ('
+    sql += '             SELECT MIN(MONTH(date)) FROM historic '
+    sql += '             WHERE YEAR(date)=2020'
+    sql += '           ) ' 
+    sql += '     ) p ON p.mois = a.mois '
 
-// });
+    let sqlValues = []
+    //   Object.keys(req.query).filter((key) => req.query[key]!=='all').map((key) => {
+    //     if(key==='annee'){
+    //       sql += ` AND YEAR(date) = ? `      
+    //       sqlValues.push(req.query[key])
+    //     }else if(key==='champs'){
+    //       sql += ` AND champs = ? `      
+    //       sqlValues.push(req.query[key])
+    //     }
+    //   })
 
-// router.get('/historicH', (req, resp) => {
-//     let sql = 'SELECT COUNT(*), button'
-//         sql += ' FROM historic WHERE MONTH(date) = MONTH(CURDATE())'
-//         sql += ' AND NOT idgasi = "a"'
-//         sql += ' GROUP BY button'
-//     // connection.query('SELECT COUNT(button) as Jalon FROM historic WHERE button = "Jalons"', (err, results) => {
-//   connection_pool.getConnection(function(error, conn) {
-//     if (error) throw err; // not connected!
+    // sql += '       AND champs="APE L\'EPERON" '
+    // sql += '       GROUP BY mois, button'
+    // sql += '   ) x'
+    // sql += '   GROUP BY x.mois'
+    // sql += ' ) y RIGHT JOIN historic_calc c on c.mois= y.mois'
+    // sql += '   WHERE c.mois <= ('
+    // sql += '                     SELECT MAX(MONTH(date)) FROM historic '
+    // sql += '                     WHERE YEAR(date)=2020'
+    // sql += '                   ) '
+    // sql += '  AND c.mois >= ('
+    // sql += '                     SELECT MIN(MONTH(date)) FROM historic '
+    // sql += '                     WHERE YEAR(date)=2020'
+    // sql += '                   ) ' 
 
-//     // Use the connection
-//     conn.query(sql, (err, result) => {
-//       // When done with the connection, release it.
-//       conn.release();
+  connection_pool.getConnection(function(error, conn) {
+    if (error) throw err; // not connected!
 
-//       // Handle error after the release.
-//       if (err) {
-//         return  resp.status(500).json({
-//                 err: "true", 
-//                 error: err.message,
-//                 errno: err.errno,
-//                 sql: err.sql,
-//                 });
-//       }
-//       else {
-//         return  resp
-//                 .status(201)
-//                 .json(result);
-//       }
-//     })
-//   });
+    // Use the connection
+    conn.query(sql, sqlValues, (err, result) => {
+      // When done with the connection, release it.
+      conn.release();
 
-// });
+      // Handle error after the release.
+      if (err) {
+        console.log(err)
+        return  resp.status(500).json({
+                err: "true", 
+                error: err.message,
+                errno: err.errno,
+                sql: err.sql,
+                });
+      }
+      else {
+        return  resp
+                .status(201)
+                .json(result);
+      }
+    })
+  });
+
+});
+
+
+
+
+router.get('/historicNbPersonne', (req, resp) => {
+
+  // let sql = 'SELECT MONTH(date) AS mois, button AS BT, COUNT(*) as NB FROM historic GROUP BY mois, BT '
+
+  let sql = ' SELECT c.mois,'
+    sql += '   CASE WHEN y.Contacts IS NULL THEN 0 ELSE y.Contacts END "Contacts", '
+    sql += '   CASE WHEN y.Diagnostic IS NULL THEN 0 ELSE y.Diagnostic END "Diagnostic", '
+    sql += '   CASE WHEN y.EFO IS NULL THEN 0 ELSE y.EFO END "EFO", '
+    sql += '   CASE WHEN y.Jalons IS NULL THEN 0 ELSE y.Jalons END "Jalons", '
+    sql += '   CASE WHEN y.Prestations IS NULL THEN 0 ELSE y.Prestations END "Prestations"'
+    sql += ' FROM '
+    sql += ' ('
+    sql += '   SELECT x.mois,'
+    sql += '     MAX(CASE WHEN x.button = "Contacts" THEN x.nbclic ELSE 0 END) "Contacts",'
+    sql += '     MAX(CASE WHEN x.button = "Diagnostic" THEN x.nbclic ELSE 0 END) "Diagnostic",'
+    sql += '     MAX(CASE WHEN x.button = "EFO" THEN x.nbclic ELSE 0 END) "EFO",'
+    sql += '     MAX(CASE WHEN x.button = "Jalons" THEN x.nbclic ELSE 0 END) "Jalons",'
+    sql += '     MAX(CASE WHEN x.button = "Prestations" THEN x.nbclic ELSE 0 END) "Prestations"'
+    sql += '   FROM'
+    sql += '   ('
+    sql += '     SELECT MONTH(date) as mois, button, count(DISTINCT(idgasi)) as nbclic FROM historic WHERE '
+    sql += '       idgasi NOT in ("icbl0540", "irle5360", "admin")' // Liste des noms à enlever (DPR)
+    // sql += '       AND YEAR(date)=2020 '
+
+    let sqlValues = []
+      Object.keys(req.query).filter((key) => req.query[key]!=='all').map((key) => {
+        if(key==='annee'){
+          sql += ` AND YEAR(date) = ? `      
+          sqlValues.push(req.query[key])
+        }else if(key==='champs'){
+          sql += ` AND champs = ? `      
+          sqlValues.push(req.query[key])
+        }
+      })
+
+    // sql += '       AND champs="APE L\'EPERON" '
+    sql += '       GROUP BY mois, button'
+    sql += '   ) x'
+    sql += '   GROUP BY x.mois'
+    sql += ' ) y RIGHT JOIN historic_calc c on c.mois= y.mois'
+    sql += '   WHERE c.mois <= ('
+    sql += '                     SELECT MAX(MONTH(date)) FROM historic '
+    sql += '                     WHERE YEAR(date)=2020'
+    sql += '                   ) '
+    sql += '  AND c.mois >= ('
+    sql += '                     SELECT MIN(MONTH(date)) FROM historic '
+    sql += '                     WHERE YEAR(date)=2020'
+    sql += '                   ) ' 
+
+  connection_pool.getConnection(function(error, conn) {
+    if (error) throw err; // not connected!
+
+    // Use the connection
+    conn.query(sql, sqlValues, (err, result) => {
+      // When done with the connection, release it.
+      conn.release();
+
+      // Handle error after the release.
+      if (err) {
+        console.log(err)
+        return  resp.status(500).json({
+                err: "true", 
+                error: err.message,
+                errno: err.errno,
+                sql: err.sql,
+                });
+      }
+      else {
+        return  resp
+                .status(201)
+                .json(result);
+      }
+    })
+  });
+
+});
+
+
+
+
+router.get('/historicClick', (req, resp) => {
+
+  // let sql = 'SELECT MONTH(date) AS mois, button AS BT, COUNT(*) as NB FROM historic GROUP BY mois, BT '
+
+  let sql = ' SELECT c.mois,'
+    sql += '   CASE WHEN y.Contacts IS NULL THEN 0 ELSE y.Contacts END "Contacts", '
+    sql += '   CASE WHEN y.Diagnostic IS NULL THEN 0 ELSE y.Diagnostic END "Diagnostic", '
+    sql += '   CASE WHEN y.EFO IS NULL THEN 0 ELSE y.EFO END "EFO", '
+    sql += '   CASE WHEN y.Jalons IS NULL THEN 0 ELSE y.Jalons END "Jalons", '
+    sql += '   CASE WHEN y.Prestations IS NULL THEN 0 ELSE y.Prestations END "Prestations"'
+    sql += ' FROM '
+    sql += ' ('
+    sql += '   SELECT x.mois,'
+    sql += '     MAX(CASE WHEN x.button = "Contacts" THEN x.nbclic ELSE 0 END) "Contacts",'
+    sql += '     MAX(CASE WHEN x.button = "Diagnostic" THEN x.nbclic ELSE 0 END) "Diagnostic",'
+    sql += '     MAX(CASE WHEN x.button = "EFO" THEN x.nbclic ELSE 0 END) "EFO",'
+    sql += '     MAX(CASE WHEN x.button = "Jalons" THEN x.nbclic ELSE 0 END) "Jalons",'
+    sql += '     MAX(CASE WHEN x.button = "Prestations" THEN x.nbclic ELSE 0 END) "Prestations"'
+    sql += '   FROM'
+    sql += '   ('
+    sql += '     SELECT MONTH(date) as mois, button, count(id) as nbclic FROM historic WHERE '
+    sql += '       idgasi NOT in ("icbl0540", "irle5360", "admin")' // Liste des noms à enlever (DPR)
+    // sql += '       AND YEAR(date)=2020 '
+
+    let sqlValues = []
+      Object.keys(req.query).filter((key) => req.query[key]!=='all').map((key) => {
+        if(key==='annee'){
+          sql += ` AND YEAR(date) = ? `      
+          sqlValues.push(req.query[key])
+        }else if(key==='champs'){
+          sql += ` AND champs = ? `      
+          sqlValues.push(req.query[key])
+        }
+      })
+
+    // sql += '       AND champs="APE L\'EPERON" '
+    sql += '       GROUP BY mois, button'
+    sql += '   ) x'
+    sql += '   GROUP BY x.mois'
+    sql += ' ) y RIGHT JOIN historic_calc c on c.mois= y.mois'
+    sql += '   WHERE c.mois <= ('
+    sql += '                     SELECT MAX(MONTH(date)) FROM historic '
+    sql += '                     WHERE YEAR(date)=2020'
+    sql += '                   ) '
+    sql += '  AND c.mois >= ('
+    sql += '                     SELECT MIN(MONTH(date)) FROM historic '
+    sql += '                     WHERE YEAR(date)=2020'
+    sql += '                   ) ' 
+
+  connection_pool.getConnection(function(error, conn) {
+    if (error) throw err; // not connected!
+
+    // Use the connection
+    conn.query(sql, sqlValues, (err, result) => {
+      // When done with the connection, release it.
+      conn.release();
+
+      // Handle error after the release.
+      if (err) {
+        console.log(err)
+        return  resp.status(500).json({
+                err: "true", 
+                error: err.message,
+                errno: err.errno,
+                sql: err.sql,
+                });
+      }
+      else {
+        return  resp
+                .status(201)
+                .json(result);
+      }
+    })
+  });
+
+});
+
+//liste filter ref
+//http://localhost:5000/activites/listeref?
+router.get('/listechamps', passport.authenticate('jwt', { session:  false }), (req,resp) =>{
+  const query = req.query;
+
+  let sql = "SELECT DISTINCT(champs) FROM `historic`"
+
+  connection_pool.getConnection(function(error, conn) {
+    if (error) throw err; // not connected!
+
+    conn.query(sql, (err, result) => {
+    // When done with the connection, release it.
+      conn.release();
+
+      // Handle error after the release.
+      if (err){
+        console.log(err.sqlMessage)
+        return  resp.status(500).json({
+                err: "true", 
+                error: err.message,
+                errno: err.errno,
+                sql: err.sql,
+                });
+      }else{
+        resp.status(201).json(result)
+      }
+
+    // Don't use the connection here, it has been returned to the pool.
+    });   
+  });
+})
+
+
 
 
 router.get('/nbligne', passport.authenticate('jwt', { session:  false }), (req,resp) =>{
